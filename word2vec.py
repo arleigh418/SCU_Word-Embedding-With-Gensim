60504c1f687899aaa6629e6b8b5a3f9cd87fb29c
張峮瑋(Arleigh Chang) <40189046+arleigh418@users.noreply.github.com>
2019-09-03 10:54:55 +0800

    Update word2vec.py

--- word2vec.py
+++ word2vec.py
@@ -2,44 +2,27 @@ import pandas as pd
 from gensim.models import Word2Vec
 import jieba
 
-# with open ('PPT-movie-7200-7275.txt','r',encoding  = 'utf-8') as f:
-#     f1 = f.read()
 
 
-f1 = pd.read_excel('your_file.xlsx')
 
-word = f1['內容']
+article = pd.read_excel('Cut_Finish_jieba.xlsx')
 
-words = []
-for i in word:
-   words.append(i)
-words = str(words)
+sentences = article['內容'].tolist()
 
+split_sentences = []
 
-sentence = list(jieba.cut(words))
-# sentence
-
+for i in sentences:
+    split_sentences.append(i.split(' '))
 
 # build a Word2Vce model
-model = Word2Vec([sentence], size=300, window=5, min_count=5, workers=4, iter=100)
-# save model to file
-# model.save("word2vec_indexasia.model")
-# load model to python
-model = Word2Vec.load("word2vec_indexasia.model")
+model = Word2Vec(split_sentences, size=500, window=10, min_count=5, workers=4)
+
+model.save("word2vec_stock.model")
 
 
 
-
-
-
-
-
-
-# print(model.wv['籃球'])
-# print(model.wv['劇情'])
-# print(model.wv['導演'])
-
-# model.most_similar("電影", topn=5)
-# model.most_similar("劇情", topn=5)
-# model.most_similar("導演", topn=5)
-
+print(model.most_similar("台積電", topn=5))
+print(model.most_similar("鴻海", topn=5))
+print(model.most_similar("中華電信", topn=5))
+print(model.most_similar("仁寶", topn=5))
+print(model.most_similar("兆豐金", topn=5))
